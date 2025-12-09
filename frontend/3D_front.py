787007bf18db6172393fdc214f91aa0bb581b980
kingchang <nuj623@gmail.com>
2025-12-09 09:47:11 +0900

    3d_front.py update

--- frontend/3D_front.py
+++ frontend/3D_front.py
@@ -6,6 +6,13 @@ prompt = st.text_input("만들 모델을 자세히 설명해 주세요.")
 
 if st.botton("생성하기"):
     with st.spinner("생성 중 입니다...."):
-              #백엔드 주소.ㄱ
-        re = requests.post(" ",json={"prompt"=prompt})
+              #백엔드 주소
+        re = requests.post("",json={"prompt"=prompt})
+        result = res.json()
+
+    st.success("생성 완료!")
+
+    st.image(result["image"])
+
+    
         
